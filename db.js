77d21bf747e059d8c8f35b7da1c8ca3c4e786f7f
YASH KUMAR PODDAR <yashkr114@gmail.com>
2025-09-01 00:13:42 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+// db.js
+
+require('dotenv').config(); // Load environment variables from .env file
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT ,
+});
+module.exports = pool;
